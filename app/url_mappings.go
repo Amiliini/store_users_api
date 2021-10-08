d45538eab5d6ad323d0a0c2e353237fc0f4f9564
MariJSin <62051165+Amiliini@users.noreply.github.com>
2021-10-08 15:30:26 +0300

    basic structure, server up
    gin-gonic server up, some paths partly done

---
+++ app/url_mappings.go
@@ -0,0 +1,14 @@
+package app
+
+import (
+	"kauppa/controllers/ping"
+	"kauppa/controllers/users"
+)
+
+func mapUrls() {
+	router.GET("/ping", ping.Ping)
+
+	router.GET("/users/:user_id", users.GetUser)
+	//router.GET("/users/search", controllers.SearchUser)
+	router.POST("/users", users.CreateUser)
+}
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"kauppa/app"
+	"kauppa/woosah"
+)
+
+func main() {
+	fmt.Println(woosah.Greet2())
+	app.StartApplication()
+}
